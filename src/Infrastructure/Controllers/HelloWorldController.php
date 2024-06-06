980758e7f7bbff4e42b5531c77c9a55b7f887a05
ArtSoller <art-sav2002@yandex.ru>
2024-06-06 23:22:50 +0700

    Mil 2 - Task 5 - src/Infrastructure/ - Add RefreshToken in Entity and create 2 controllers - for login and helo world

---
+++ src/Infrastructure/Controllers/HelloWorldController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Infrastructure\Controllers;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+class HelloWorldController extends AbstractController
+{
+
+    #[Route('/api/helloworld', name: 'helloworld')]
+    public function helloWorld(): Response
+    {
+        return new Response('<http> Hello World! </http>');
+    }
+}
