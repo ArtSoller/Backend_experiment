643fa724a8d8c084f017a00fb2ddf30a87c2777e
ArtSoller <art-sav2002@yandex.ru>
2024-06-11 23:21:14 +0700

    Mil 2 - Task 6 - src/Infrastructure/Controllers/Admin/DashboardController.php - Add prototype of Dashboard controller

---
+++ src/Infrastructure/Controllers/Admin/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Infrastructure\Controllers\Admin;
+
+use App\Infrastructure\Database\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
