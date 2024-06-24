4ed5a3878c1e8b03f907e14dbb4b2fa513bee01d
ArtSoller <art-sav2002@yandex.ru>
2024-06-24 22:18:57 +0700

    Mil 2 - Task 6 - src/Infrastructure/Database/* - Add ULID to user_id, change columns for Users, work with unique or not unique field, add existing user check

--- src/Infrastructure/Database/Entity/Users.php
+++ src/Infrastructure/Database/Entity/Users.php
@@ -3,35 +3,47 @@
 namespace App\Infrastructure\Database\Entity;
 
 use App\Infrastructure\Database\Repository\UsersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UlidType;
+use Symfony\Component\Uid\Ulid;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
-class Users implements \Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id_user = null;
+    #[ORM\Column(type: UlidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.ulid_generator')]
+    private ?Ulid $user_id = null;
 
     #[ORM\Column(length: 255)]
     private ?string $username = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column]
     private ?string $password = null;
 
     #[ORM\JoinTable(name: 'users_roles')]
-    #[ORM\JoinColumn(name: 'id_user', referencedColumnName: 'id_user')]
-    #[ORM\InverseJoinColumn(name: 'id_role', referencedColumnName: 'id_role')]
+    #[ORM\JoinColumn(name: '$user_id', referencedColumnName: '$user_id')]
+    #[ORM\InverseJoinColumn(name: 'role_id', referencedColumnName: 'role_id')]
     #[ORM\ManyToMany(targetEntity: Roles::class)]
     private Collection $users_roles;
 
-    public function getId_user(): ?int
+    public function __construct()
     {
-        return $this->id_user;
+        $this->user_roles = new ArrayCollection();
+    }
+    public function getId(): ?Ulid
+    {
+        return $this->user_id;
     }
 
     public function getUsername(): ?string
@@ -58,6 +70,24 @@ class Users implements \Symfony\Component\Security\Core\User\PasswordAuthenticat
         return $this;
     }
 
+    public function getRoles(): array
+    {
+        $user_roles[] = $this->user_roles;
+        return $user_roles;
+    }
+
+    public function setRoles(ArrayCollection $user_roles): static
+    {
+        $this->user_roles = $user_roles;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
     public function getPassword(): ?string
     {
         return $this->password;
@@ -70,15 +100,9 @@ class Users implements \Symfony\Component\Security\Core\User\PasswordAuthenticat
         return $this;
     }
 
-    public function getUsersRoles(): ArrayCollection
-    {
-        return $this->users_roles;
-    }
-
-    public function setUsersRoles(ArrayCollection $users_roles): static
-    {
-        $this->users_roles = $users_roles;
-
-        return $this;
-    }
+//    public function eraseCredentials(): void
+//    {
+//        // If you store any temporary, sensitive data on the user, clear it here
+//        // $this->plainPassword = null;
+//    }
 }
