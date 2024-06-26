7563d53f7cbf0fc870c42101ffaa0b3b91d00192
ArtSoller <art-sav2002@yandex.ru>
2024-06-26 22:55:04 +0700

    Mil 2 - Task 10 - src/Infrastructure/Database/Users.php - Add relationship between Users and Currencies

--- src/Infrastructure/Database/Entity/Users.php
+++ src/Infrastructure/Database/Entity/Users.php
@@ -3,14 +3,14 @@
 namespace App\Infrastructure\Database\Entity;
 
 use App\Infrastructure\Database\Repository\UsersRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UlidType;
-use Symfony\Component\Uid\Ulid;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-
-
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Uid\Ulid;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
@@ -31,17 +31,29 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\JoinTable(name: 'users_roles')]
-    #[ORM\JoinColumn(name: '$user_id', referencedColumnName: '$user_id')]
+    #[ORM\JoinTable(name: 'user_roles')]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'user_id')]
     #[ORM\InverseJoinColumn(name: 'role_id', referencedColumnName: 'role_id')]
     #[ORM\ManyToMany(targetEntity: Roles::class)]
-    private Collection $users_roles;
+    private Collection $user_roles;
+
+    #[ORM\JoinTable(name: 'alerts')]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'user_id')]
+    #[ORM\InverseJoinColumn(name: 'currency_id', referencedColumnName: 'currency_id')]
+    #[ORM\ManyToMany(targetEntity: Currencies::class)]
+    private Collection $alerts;
+
+//    #[ORM\ManyToOne(targetEntity: Subscriptions::class, inversedBy: "users")]
+//    #[ORM\JoinColumn(name: "subscription_id", referencedColumnName: "sub_id", nullable: true)]
+//    private Collection $subscription;
 
     public function __construct()
     {
         $this->user_roles = new ArrayCollection();
+        $this->user_alerts = new ArrayCollection();
+//        $this->subscription = new ArrayCollection();
     }
-    public function getId(): ?Ulid
+    public function getUserId(): ?Ulid
     {
         return $this->user_id;
     }
@@ -70,25 +82,61 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getUserRoles(): Collection
+    {
+        return $this->user_roles;
+    }
     public function getRoles(): array
     {
-        $user_roles[] = $this->user_roles;
-        return $user_roles;
+        $roles = $this->getUserRoles()->map(function($role) {
+            return $role->getName();
+        })->toArray();
+
+        $roles[] = 'ROLE_USER';
+
+        return $roles;
     }
 
-    public function setRoles(ArrayCollection $user_roles): static
+    public function setUserRoles(Roles $user_role): static
     {
-        $this->user_roles = $user_roles;
+        if (!$this->user_roles->contains($user_role)) {
+            $this->user_roles->add($user_role);
+        }
 
         return $this;
     }
+
+    public function getAlerts(): ArrayCollection
+    {
+        return $this->alerts;
+    }
+
+    public function setAlerts(ArrayCollection $alerts): static
+    {
+        $this->alerts = $alerts;
+
+        return $this;
+    }
+
+//    public function getSubscription(): ArrayCollection
+//    {
+//        return $this->subscription;
+//    }
+//
+//    public function setSubscription(ArrayCollection $subscription): static
+//    {
+//        $this->subscription = $subscription;
+//
+//        return $this;
+//    }
+
 
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
 
-    public function getPassword(): ?string
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -100,9 +148,9 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-//    public function eraseCredentials(): void
-//    {
-//        // If you store any temporary, sensitive data on the user, clear it here
-//        // $this->plainPassword = null;
-//    }
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 }
