37d94899d966881bfb382ed3505a5a0d286d75be
ArtSoller <art-sav2002@yandex.ru>
2024-06-26 22:58:05 +0700

    Mil 2 - Task 10 - src/Infrastructure/Controllers/Admin/DashboardController.php - Add functionality for dashboard page

--- src/Infrastructure/Controllers/Admin/DashboardController.php
+++ src/Infrastructure/Controllers/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Infrastructure\Controllers\Admin;
 
+use App\Infrastructure\Database\Entity\Currencies;
+use App\Infrastructure\Database\Entity\Subscriptions;
+
+use App\Infrastructure\Database\Entity\News;
 use App\Infrastructure\Database\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -9,3 +13,29 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('Admin/admin.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Admin Dashboard');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Entities');
+//        yield MenuItem::linkToCrud('Currencies', 'fas fa-coins', Currencies::class);
+        //        yield MenuItem::linkToCrud('Subscriptions', 'fas fa-bell', Subscriptions::class);
+
+        yield MenuItem::linkToCrud('News', 'fas fa-newspaper', News::class);
+        yield MenuItem::section('Users Management');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', Users::class);
+    }
+}
