8212897210ff715b577b156daa5900bd09877af6
ArtSoller <art-sav2002@yandex.ru>
2024-06-26 23:01:01 +0700

    Mil 2 - Task 6 - src/Infrastructure/Controllers/Api/version1/* - Change name for getid and Route

--- src/Infrastructure/Controllers/Api/version1/LoginController.php
+++ src/Infrastructure/Controllers/Api/version1/LoginController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class LoginController extends AbstractController
 {
-    #[Route('/api/v1/login', name: 'api_v1_login', methods: ['GET'])]
+    #[Route('/api/version1/login', name: 'api_v1_login', methods: ['GET'])]
     public function login(Request $request,
                           UserPasswordHasherInterface $passwordHasher,
                           EntityManagerInterface $entityManager,
@@ -35,7 +35,7 @@ class LoginController extends AbstractController
 
         $token = $jwtManager->createFromPayload($user,
             ['expires_in' => '3600',
-                'user_id' => $user->getId()->toRfc4122(), # 01HZQTM6EY3Y23J57HN9207V99 -> 018fee3d-8585-7020-77e6-e68c66e393b2
+                'user_id' => $user->getUserId()->toRfc4122(), # 01HZQTM6EY3Y23J57HN9207V99 -> 018fee3d-8585-7020-77e6-e68c66e393b2
                 'email' => $email]);
         return $this->json(['message' => 'User authenticated successfully',
             'access_token' => $token],
