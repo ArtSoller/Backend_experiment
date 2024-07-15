479b80ebf587e309fc1632344ad19877524ffa7d
Артем Саляев <art-sav2002@yandex.ru>
2024-07-15 18:02:14 +0700

    Mil 2 - Task 16 - src/Infrastructure/Controllers/Api/version1/CurrenciesController.php - Create GET Currencies controller for search

---
+++ src/Infrastructure/Controllers/Api/version1/CurrenciesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Infrastructure\Controllers\Api\version1;
+
+use App\Infrastructure\Database\Entity\Currencies;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CurrenciesController extends AbstractController
+{
+    #[Route('/api/version1/currencies', name: 'api_version1_currencies', methods: ['GET'])]
+    public function searchCountries(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $searchTerm = $request->query->get('search');
+
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder->select('currency')
+            ->from(Currencies::class, 'currency')
+            ->where('currency.name LIKE :searchTerm')
+            ->setParameter('searchTerm', '%' . $searchTerm . '%');
+
+        $currencies = $queryBuilder->getQuery()->getResult();
+
+        $response = [];
+        foreach ($currencies as $currency) {
+            $response[] = [
+                'id' => $currency->getCountryId(),
+                'name' => $currency->getName(),
+            ];
+        }
+
+        return $this->json($response);
+    }
+}
