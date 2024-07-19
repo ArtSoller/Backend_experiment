f661f5ad0edde61184a5bc13532972392e87ce14
ArtSoller <art-sav2002@yandex.ru>
2024-07-19 21:24:23 +0700

    Mil 2 - Task 16 -  src/features/UserListFeatures/AddToListModalFields/AddToListModalFields.tsx - Add controller to change the activity status of a rule

---
+++ src/Infrastructure/Controllers/Api/version1/ActivityRuleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Controllers\Api\version1;
+
+use App\Infrastructure\Database\Entity\Rules;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActivityRuleController extends AbstractController
+{
+    #[Route('/api/version1/change_activity_rules/{id}', name: 'api_version1_change_activity_rules', methods: ['PUT'])]
+    public function ChangeActivityRule(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $rule = $entityManager->getRepository(Rules::class)->find($id);
+        if (!$rule) {
+            return $this->json(['message' => 'Rule not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $rule->setRuleStatus(!$rule->getRuleStatus()); // Предполагаем, что метод setRuleStatus существует в сущности Rules
+        $entityManager->persist($rule);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Rule activity change successfully'], Response::HTTP_OK);
+    }
+}
